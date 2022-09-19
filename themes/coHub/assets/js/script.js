175be4227b6dbc5010f25cc35ce8aff948dbfb3a
Marco Gomiero <marco.gomiero.93@gmail.com>
2022-09-19 22:48:18 +0200

    Add logo. Improve navigation

--- themes/coHub/assets/js/script.js
+++ themes/coHub/assets/js/script.js
@@ -73,13 +73,13 @@ $(document).ready(function () {
 });
 
 // Add nav bg
-$(window).on("scroll", function () {
-  if ($(window).scrollTop()) {
-    $(".main-nav").addClass("nav-bg");
-  } else {
-    $(".main-nav").removeClass("nav-bg");
-  }
-});
+//$(window).on("scroll", function () {
+//  if ($(window).scrollTop()) {
+//    $(".main-nav").addClass("nav-bg");
+//  } else {
+//    $(".main-nav").removeClass("nav-bg");
+//  }
+//});
 // G-Map
 /**
  * Created by Kausar on 06/10/2016.
